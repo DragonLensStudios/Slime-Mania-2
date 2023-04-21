99677b4178b16000d14a2de7237f065618c87a2a
Mony Dragon <monydragon@hotmail.com>
2023-04-21 15:27:53 -0500

    Added LootLocker SDK and Configured Project

---
+++ Assets/LootLockerSDK/Samples/LootLockerExamples/Scripts/GuestLoginUniqueIdentifier.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LootLocker.Requests;
+using UnityEngine.UI;
+
+namespace LootLocker {
+public class GuestLoginUniqueIdentifier : MonoBehaviour
+{
+    public Text loginInformationText;
+    public Text playerIdText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* 
+         * Override settings to use the Example games setup
+         */
+        //LootLockerSettingsOverrider.OverrideSettings();
+
+        /* Start guest session with an unique identifier tied to this device.
+         * So if someone uninstall your game, they will be able to log in again when they reinstall to the 
+         * same account as long as they are using the same device.
+         */
+        LootLockerSDKManager.StartGuestSession(SystemInfo.deviceUniqueIdentifier, (response) =>
+        {
+            if(response.success)
+            {
+
+                loginInformationText.text = "Guest session started";
+                LootLockerSDKManager.SetPlayerName("Mony", (response) =>
+                {
+                    if (response.success)
+                    {
+                        Debug.Log("Successfully set player name");
+                        playerIdText.text = $"Player Name: {response.name}";
+
+                    } else
+                    {
+                        Debug.Log("Error setting player name");
+                    }
+                });
+            }
+            else
+            {
+                loginInformationText.text = "Error" + response.Error;
+            }
+        });
+    }
+}
+}
